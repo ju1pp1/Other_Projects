47addbe205d1218bd76db28b98361c37c2f16ba9
ju1pp1 <jeremicraft@hotmail.com>
2022-10-28 14:42:20 +0300

    School projects

---
+++ CommonClasses/TLLibrary/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TLLibrary
+{
+    /// <summary>
+    /// Testiluokka yleiskäyttöön
+    /// </summary>
+    public class Class1
+    {
+        int numero;
+        /// <summary>
+        /// Numero, luettaessa yhtä suurempi
+        /// </summary>
+        public int Numero
+        {
+            get => numero+1;
+            set => numero = value;
+        }
+
+        /// <summary>
+        /// Tulostaa
+        /// </summary>
+        /// <returns>Tulostettava teksti</returns>
+        public string Tulosta()
+        {
+            return $"It Works! Numero={Numero}";
+        }
+    }
+}
