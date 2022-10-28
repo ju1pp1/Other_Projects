054572aa599ecc3c83b446f2da348acde052444a
ju1pp1 <jeremicraft@hotmail.com>
2022-10-28 14:36:01 +0300

    School projects

---
+++ Coop1/Coop1/Pelaaja.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coop1
+{
+    class Pelaaja
+    {
+        string alias;
+        int pisteet;
+        //ctor<tab>
+        public Pelaaja()
+        {
+
+        }
+        public string Alias { get => alias; set => alias = value; }
+        public int Pisteet { get => pisteet; set => pisteet = value; }
+    }
+}
---
+++ Coop1/Coop1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coop1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pelaaja guru = new Pelaaja();
+            guru.Alias = "Guru";
+            guru.Pisteet = 100;
+            
+            Pelaaja p = new Pelaaja();
+            p.Alias = "ActionMan";
+            p.Pisteet = 10;
+            Tulostaja tulostaja = new Tulostaja();
+            tulostaja.setGuru(guru);
+
+            tulostaja.Tulosta(p);
+
+            Pelaaja p2 = new Pelaaja();
+            p2.Alias = "ActionWoMan";
+            p2.Pisteet = 5;
+            tulostaja.Tulosta(p2);
+        }
+    }
+}
