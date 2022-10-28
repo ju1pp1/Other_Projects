0de919f384c4a214460ee3d546afb956f6696a91
ju1pp1 <jeremicraft@hotmail.com>
2022-10-28 14:37:23 +0300

    School projects

---
+++ EsimLuokat/EsimLuokat/Kokonaisuus.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsimLuokat
+{
+    class Kokonaisuus
+    {
+        Osa osa;
+        Osa varaosa;
+        // tee tälle: LisääVaraosa 
+        // ja päivitä Tulosta-metodi
+
+        // Lisää osa
+        public void LisääOsa(Osa osa)
+        {
+            this.osa = osa;
+        }
+        public void LisääVaraOsa(Osa osa)
+        {
+            this.varaosa = osa;
+        }
+        // Tulosta
+        public string Tulosta()
+        {
+            return "K: "+osa.Tulosta()+" v= "+varaosa.Tulosta();
+        }
+    }
+}
---
+++ EsimLuokat/EsimLuokat/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsimLuokat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kokonaisuus kok = new Kokonaisuus();
+            Osa osa = new Osa();
+            osa.Luku = 42;
+            kok.LisääOsa(osa);
+            // tee toinen Osa
+            Osa vOsa = new Osa();
+            vOsa.Luku = 21; // anna arvo Luku:lle
+            // ja lisää varaosa
+            kok.LisääVaraOsa(vOsa);
+
+            // tulosta
+            Console.WriteLine( osa.Tulosta());
+            Console.WriteLine(vOsa.Tulosta());
+            Console.WriteLine( kok.Tulosta());
+        }
+    }
+}
