bf1f5fa3eaa05a737396f30c085d29c347e476e8
ju1pp1 <jeremicraft@hotmail.com>
2022-10-28 14:38:59 +0300

    School projects

---
+++ List2/Lista2/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2
+{
+    class Person
+    {
+        string firstname;
+        string lastname;
+        // muodostin
+        public Person() { }
+
+        public Person(string v1, string v2)
+        {
+            firstname = v1;
+            lastname = v2;
+        }        
+
+        public string Tulosta()
+        {
+            return firstname + " " + lastname;
+        }
+
+        public void SetFirstName(string v)
+        {
+            firstname = v;
+        }
+
+        public void SetLastName(string v)
+        {
+            lastname = v;
+        }
+    }
+}
---
+++ List2/Lista2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2
+{
+    class Program
+    {
+        // pääohhelma
+        static void Main(string[] args)
+        {
+            List<Person> lista = new List<Person>();
+            Person pertti = new Person();
+            // aseta etunimi ja sukunimi
+            pertti.SetFirstName("Pertti");
+            pertti.SetLastName("Pasanen");
+            lista.Add(pertti);
+            // toinen vielä
+            lista.Add(new Person("Vertti", "Velmula"));
+
+            for (int i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine(lista[i].Tulosta());
+            }
+        }
+    }
+}
